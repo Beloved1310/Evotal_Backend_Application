fd46fad408484f4e9cd92c09313953d153e5a7e6
Beloved1310 <adeoluwafisayomi@gmail.com>
2023-02-07 23:11:29 -0800

    user routes

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json({ limit: '50mb' }))
 app.use('/evotal', evotal)
 app.use('/medication', medication)
-app.use('/', user)
+app.use('/user', user)
 
 app.listen(PORT, () => {
   console.log(`Web server is running ${PORT}`)
