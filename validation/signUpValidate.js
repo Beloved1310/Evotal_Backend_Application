4e64bc6919414b66cf3258ed5525f1d4a5fbdb46
Beloved1310 <adeoluwafisayomi@gmail.com>
2023-02-11 12:44:36 -0800

    changes

--- validation/signUpValidate.js
+++ validation/signUpValidate.js
@@ -5,8 +5,7 @@ module.exports = function validate(input) {
   const schema = Joi.object({
     fullname: Joi.string().min(5).trim().required(),
     email: Joi.string().email().min(3).max(50).lowercase().required().trim(),
-    password: Joi.string().required()
-      
+    password: Joi.string().required()  
   })
   return schema.validate(input)
 }
